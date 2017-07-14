988533aa3570890d937a900ff84bb93a8fd7373a
CortexPE <CortexPE@users.noreply.github.com>
2017-07-13 21:52:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 DIR="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 
+# Automatically restart the server
 DO_LOOP="no"
 
 while getopts "p:f:l" OPTION 2> /dev/null; do
@@ -36,10 +37,12 @@ fi
 if [ "$POCKETMINE_FILE" == "" ]; then
 	if [ -f ./PocketMine-MP.phar ]; then
 		POCKETMINE_FILE="./PocketMine-MP.phar"
+	if [ -f ./Leveryl*.phar ]; then
+		POCKETMINE_FILE="./Leveryl*.phar"
 	elif [ -f ./src/pocketmine/PocketMine.php ]; then
 		POCKETMINE_FILE="./src/pocketmine/PocketMine.php"
 	else
-		echo "Couldn't find a valid PocketMine-MP installation"
+		echo "Couldn't find a valid Leveryl installation"
 		exit 1
 	fi
 fi
@@ -57,9 +60,9 @@ while [ "$LOOPS" -eq 0 ] || [ "$DO_LOOP" == "yes" ]; do
 		if [ ${LOOPS} -gt 0 ]; then
 			echo "Restarted $LOOPS times"
 		fi 
-		echo "To escape the loop, press CTRL+C now. Otherwise, wait 5 seconds for the server to restart."
+		echo "To escape the loop, press CTRL+C now. Otherwise, wait 2 seconds for the server to restart."
 		echo ""
-		sleep 5
+		sleep 2
 		((LOOPS++))
 	fi
 done
