b3288e806cec0734c6918537d1310959b172baf6
NycuRO <nghergu@yahoo.com>
2017-07-30 18:54:13 +0300

    Update TransferPacket.php

--- src/pocketmine/network/mcpe/protocol/TransferPacket.php
+++ src/pocketmine/network/mcpe/protocol/TransferPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -27,28 +27,23 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class TransferPacket extends DataPacket
-{
+class TransferPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::TRANSFER_PACKET;
 
 	public $address;
 	public $port = 19132;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->address = $this->getString();
 		$this->port = $this->getLShort();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putString($this->address);
 		$this->putLShort($this->port);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleTransfer($this);
 	}
 
