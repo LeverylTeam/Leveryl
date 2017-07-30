8f54d74ad9ef6c77c2bfe51b5d1f17047a732c91
NycuRO <nghergu@yahoo.com>
2017-07-30 18:22:30 +0300

    Update MovePlayerPacket.php

--- src/pocketmine/network/mcpe/protocol/MovePlayerPacket.php
+++ src/pocketmine/network/mcpe/protocol/MovePlayerPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class MovePlayerPacket extends DataPacket
-{
+class MovePlayerPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::MOVE_PLAYER_PACKET;
 
 	const MODE_NORMAL = 0;
@@ -45,13 +44,12 @@ class MovePlayerPacket extends DataPacket
 	public $bodyYaw;
 	public $pitch;
 	public $mode = self::MODE_NORMAL;
-	public $onGround;
+	public $onGround = false; //TODO
 	public $ridingEid = 0;
 	public $int1 = 0;
 	public $int2 = 0;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->getVector3f($this->x, $this->y, $this->z);
 		$this->pitch = $this->getLFloat();
@@ -60,15 +58,13 @@ class MovePlayerPacket extends DataPacket
 		$this->mode = $this->getByte();
 		$this->onGround = $this->getBool();
 		$this->ridingEid = $this->getEntityRuntimeId();
-		if($this->mode === MovePlayerPacket::MODE_TELEPORT) {
+		if($this->mode === MovePlayerPacket::MODE_TELEPORT){
 			$this->int1 = $this->getLInt();
 			$this->int2 = $this->getLInt();
 		}
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putVector3f($this->x, $this->y, $this->z);
 		$this->putLFloat($this->pitch);
@@ -77,14 +73,13 @@ class MovePlayerPacket extends DataPacket
 		$this->putByte($this->mode);
 		$this->putBool($this->onGround);
 		$this->putEntityRuntimeId($this->ridingEid);
-		if($this->mode === MovePlayerPacket::MODE_TELEPORT) {
+		if($this->mode === MovePlayerPacket::MODE_TELEPORT){
 			$this->putLInt($this->int1);
 			$this->putLInt($this->int2);
 		}
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleMovePlayer($this);
 	}
 
