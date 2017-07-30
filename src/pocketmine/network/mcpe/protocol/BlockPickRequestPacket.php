b211424c8cc8a9a6242d72d3ca4ce8c10cc57209
NycuRO <nghergu@yahoo.com>
2017-07-30 17:42:57 +0300

    Update BlockPickRequestPacket.php

--- src/pocketmine/network/mcpe/protocol/BlockPickRequestPacket.php
+++ src/pocketmine/network/mcpe/protocol/BlockPickRequestPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 
 namespace pocketmine\network\mcpe\protocol;
@@ -29,8 +29,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class BlockPickRequestPacket extends DataPacket
-{
+class BlockPickRequestPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::BLOCK_PICK_REQUEST_PACKET;
 
 	public $tileX;
@@ -38,21 +37,17 @@ class BlockPickRequestPacket extends DataPacket
 	public $tileZ;
 	public $hotbarSlot;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->getSignedBlockPosition($this->tileX, $this->tileY, $this->tileZ);
 		$this->hotbarSlot = $this->getByte();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putSignedBlockPosition($this->tileX, $this->tileY, $this->tileZ);
 		$this->putByte($this->hotbarSlot);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleBlockPickRequest($this);
 	}
 }
