c774608ed7c26d80782e763015964ec6202c8a93
NycuRO <nghergu@yahoo.com>
2017-07-30 18:49:39 +0300

    Update SpawnExperienceOrbPacket.php

--- src/pocketmine/network/mcpe/protocol/SpawnExperienceOrbPacket.php
+++ src/pocketmine/network/mcpe/protocol/SpawnExperienceOrbPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class SpawnExperienceOrbPacket extends DataPacket
-{
+class SpawnExperienceOrbPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::SPAWN_EXPERIENCE_ORB_PACKET;
 
 	public $x;
@@ -37,21 +36,17 @@ class SpawnExperienceOrbPacket extends DataPacket
 	public $z;
 	public $amount;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->getVector3f($this->x, $this->y, $this->z);
 		$this->amount = $this->getVarInt();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putVector3f($this->x, $this->y, $this->z);
 		$this->putVarInt($this->amount);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleSpawnExperienceOrb($this);
 	}
 }
