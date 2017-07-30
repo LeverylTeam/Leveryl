6de2d6f9a56523975d7efd3861453f60a62f07ba
NycuRO <nghergu@yahoo.com>
2017-07-30 18:34:45 +0300

    Update ResourcePackClientResponsePacket.php

--- src/pocketmine/network/mcpe/protocol/ResourcePackClientResponsePacket.php
+++ src/pocketmine/network/mcpe/protocol/ResourcePackClientResponsePacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class ResourcePackClientResponsePacket extends DataPacket
-{
+class ResourcePackClientResponsePacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::RESOURCE_PACK_CLIENT_RESPONSE_PACKET;
 
 	const STATUS_REFUSED = 1;
@@ -40,27 +39,23 @@ class ResourcePackClientResponsePacket extends DataPacket
 	public $status;
 	public $packIds = [];
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->status = $this->getByte();
 		$entryCount = $this->getLShort();
-		while($entryCount-- > 0) {
+		while($entryCount-- > 0){
 			$this->packIds[] = $this->getString();
 		}
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putByte($this->status);
 		$this->putLShort(count($this->packIds));
-		foreach($this->packIds as $id) {
+		foreach($this->packIds as $id){
 			$this->putString($id);
 		}
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleResourcePackClientResponse($this);
 	}
 
