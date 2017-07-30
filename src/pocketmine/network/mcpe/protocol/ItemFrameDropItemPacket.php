a19942f4d91f96dda6e64472edefd9de31d4f6b1
NycuRO <nghergu@yahoo.com>
2017-07-30 18:12:36 +0300

    Update ItemFrameDropItemPacket.php

--- src/pocketmine/network/mcpe/protocol/ItemFrameDropItemPacket.php
+++ src/pocketmine/network/mcpe/protocol/ItemFrameDropItemPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -27,8 +27,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class ItemFrameDropItemPacket extends DataPacket
-{
+class ItemFrameDropItemPacket extends DataPacket{
 
 	const NETWORK_ID = ProtocolInfo::ITEM_FRAME_DROP_ITEM_PACKET;
 
@@ -36,19 +35,15 @@ class ItemFrameDropItemPacket extends DataPacket
 	public $y;
 	public $z;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->getBlockPosition($this->x, $this->y, $this->z);
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putBlockPosition($this->x, $this->y, $this->z);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleItemFrameDropItem($this);
 	}
 
