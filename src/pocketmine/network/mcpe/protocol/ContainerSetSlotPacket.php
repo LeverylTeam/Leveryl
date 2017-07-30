7a43d19ef3b4e6176a6298fa654c404f763c7d78
NycuRO <nghergu@yahoo.com>
2017-07-30 18:01:33 +0300

    Update ContainerSetSlotPacket.php

--- src/pocketmine/network/mcpe/protocol/ContainerSetSlotPacket.php
+++ src/pocketmine/network/mcpe/protocol/ContainerSetSlotPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 use pocketmine\item\Item;
 use pocketmine\network\mcpe\NetworkSession;
 
-class ContainerSetSlotPacket extends DataPacket
-{
+class ContainerSetSlotPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::CONTAINER_SET_SLOT_PACKET;
 
 	public $windowid;
@@ -39,8 +38,7 @@ class ContainerSetSlotPacket extends DataPacket
 	public $item;
 	public $selectSlot = 0;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->windowid = $this->getByte();
 		$this->slot = $this->getVarInt();
 		$this->hotbarSlot = $this->getVarInt();
@@ -48,9 +46,7 @@ class ContainerSetSlotPacket extends DataPacket
 		$this->selectSlot = $this->getByte();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putByte($this->windowid);
 		$this->putVarInt($this->slot);
 		$this->putVarInt($this->hotbarSlot);
@@ -58,8 +54,7 @@ class ContainerSetSlotPacket extends DataPacket
 		$this->putByte($this->selectSlot);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleContainerSetSlot($this);
 	}
 
