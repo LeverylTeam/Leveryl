c81f68c7110007921c4c44dcb45832694c3f49e5
NycuRO <nghergu@yahoo.com>
2017-07-30 18:20:33 +0300

    Update MobEffectPacket.php

--- src/pocketmine/network/mcpe/protocol/MobEffectPacket.php
+++ src/pocketmine/network/mcpe/protocol/MobEffectPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class MobEffectPacket extends DataPacket
-{
+class MobEffectPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::MOB_EFFECT_PACKET;
 
 	const EVENT_ADD = 1;
@@ -43,8 +42,7 @@ class MobEffectPacket extends DataPacket
 	public $particles = true;
 	public $duration = 0;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->eventId = $this->getByte();
 		$this->effectId = $this->getVarInt();
@@ -53,9 +51,7 @@ class MobEffectPacket extends DataPacket
 		$this->duration = $this->getVarInt();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putByte($this->eventId);
 		$this->putVarInt($this->effectId);
@@ -64,8 +60,7 @@ class MobEffectPacket extends DataPacket
 		$this->putVarInt($this->duration);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleMobEffect($this);
 	}
 
