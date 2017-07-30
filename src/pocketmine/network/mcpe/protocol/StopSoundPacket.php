1162f7aaae95bba90d2d8997cd1947b5b8c4f096
NycuRO <nghergu@yahoo.com>
2017-07-30 18:51:31 +0300

    Update StopSoundPacket.php

--- src/pocketmine/network/mcpe/protocol/StopSoundPacket.php
+++ src/pocketmine/network/mcpe/protocol/StopSoundPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 
 namespace pocketmine\network\mcpe\protocol;
@@ -29,28 +29,23 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class StopSoundPacket extends DataPacket
-{
+class StopSoundPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::STOP_SOUND_PACKET;
 
 	public $soundName;
 	public $stopAll;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->soundName = $this->getString();
 		$this->stopAll = $this->getBool();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putString($this->soundName);
 		$this->putBool($this->stopAll);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleStopSound($this);
 	}
 }
