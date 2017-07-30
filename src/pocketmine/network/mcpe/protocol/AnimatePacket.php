558d79f5fc6efc373e4c8cc939bc8fe7142d0668
NycuRO <nghergu@yahoo.com>
2017-07-30 17:27:40 +0300

    Update AnimatePacket.php

--- src/pocketmine/network/mcpe/protocol/AnimatePacket.php
+++ src/pocketmine/network/mcpe/protocol/AnimatePacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,35 +28,30 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class AnimatePacket extends DataPacket
-{
+class AnimatePacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::ANIMATE_PACKET;
 
 	public $action;
 	public $entityRuntimeId;
 	public $float; //TODO (Boat rowing time?)
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->action = $this->getVarInt();
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
-		if($this->action & 0x80) {
+		if($this->action & 0x80){
 			$this->float = $this->getLFloat();
 		}
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putVarInt($this->action);
 		$this->putEntityRuntimeId($this->entityRuntimeId);
-		if($this->action & 0x80) {
+		if($this->action & 0x80){
 			$this->putLFloat($this->float);
 		}
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleAnimate($this);
 	}
 
