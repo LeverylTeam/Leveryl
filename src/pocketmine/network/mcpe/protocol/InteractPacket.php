1159f692912627844154893b354b3b2bc3d9b794
NycuRO <nghergu@yahoo.com>
2017-07-30 18:11:31 +0300

    Update InteractPacket.php

--- src/pocketmine/network/mcpe/protocol/InteractPacket.php
+++ src/pocketmine/network/mcpe/protocol/InteractPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class InteractPacket extends DataPacket
-{
+class InteractPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::INTERACT_PACKET;
 
 	const ACTION_RIGHT_CLICK = 1;
@@ -42,21 +41,17 @@ class InteractPacket extends DataPacket
 	public $action;
 	public $target;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->action = $this->getByte();
 		$this->target = $this->getEntityRuntimeId();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putByte($this->action);
 		$this->putEntityRuntimeId($this->target);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleInteract($this);
 	}
 
