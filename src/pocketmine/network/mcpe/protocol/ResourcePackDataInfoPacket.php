7895aac5c396e442a570d86f84c961057483e072
NycuRO <nghergu@yahoo.com>
2017-07-30 18:35:20 +0300

    Update ResourcePackDataInfoPacket.php

--- src/pocketmine/network/mcpe/protocol/ResourcePackDataInfoPacket.php
+++ src/pocketmine/network/mcpe/protocol/ResourcePackDataInfoPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 
 namespace pocketmine\network\mcpe\protocol;
@@ -29,8 +29,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class ResourcePackDataInfoPacket extends DataPacket
-{
+class ResourcePackDataInfoPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::RESOURCE_PACK_DATA_INFO_PACKET;
 
 	public $packId;
@@ -39,8 +38,7 @@ class ResourcePackDataInfoPacket extends DataPacket
 	public $compressedPackSize;
 	public $sha256;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->packId = $this->getString();
 		$this->maxChunkSize = $this->getLInt();
 		$this->chunkCount = $this->getLInt();
@@ -48,9 +46,7 @@ class ResourcePackDataInfoPacket extends DataPacket
 		$this->sha256 = $this->getString();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putString($this->packId);
 		$this->putLInt($this->maxChunkSize);
 		$this->putLInt($this->chunkCount);
@@ -58,8 +54,7 @@ class ResourcePackDataInfoPacket extends DataPacket
 		$this->putString($this->sha256);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleResourcePackDataInfo($this);
 	}
 
