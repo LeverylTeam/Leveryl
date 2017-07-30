fa91f5fb17223dbd749493b99702ec44aaf800e2
NycuRO <nghergu@yahoo.com>
2017-07-30 18:45:14 +0300

    Update SetTitlePacket.php

--- src/pocketmine/network/mcpe/protocol/SetTitlePacket.php
+++ src/pocketmine/network/mcpe/protocol/SetTitlePacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 
 namespace pocketmine\network\mcpe\protocol;
@@ -29,8 +29,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetTitlePacket extends DataPacket
-{
+class SetTitlePacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::SET_TITLE_PACKET;
 
 	const TYPE_CLEAR_TITLE = 0;
@@ -46,8 +45,7 @@ class SetTitlePacket extends DataPacket
 	public $stayTime = 0;
 	public $fadeOutTime = 0;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->type = $this->getVarInt();
 		$this->text = $this->getString();
 		$this->fadeInTime = $this->getVarInt();
@@ -55,9 +53,7 @@ class SetTitlePacket extends DataPacket
 		$this->fadeOutTime = $this->getVarInt();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putVarInt($this->type);
 		$this->putString($this->text);
 		$this->putVarInt($this->fadeInTime);
@@ -65,8 +61,7 @@ class SetTitlePacket extends DataPacket
 		$this->putVarInt($this->fadeOutTime);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleSetTitle($this);
 	}
 }
