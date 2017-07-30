c548bb8de348f5d66fae8e8678126e50b748d25b
NycuRO <nghergu@yahoo.com>
2017-07-30 18:43:37 +0300

    Update SetSpawnPositionPacket.php

--- src/pocketmine/network/mcpe/protocol/SetSpawnPositionPacket.php
+++ src/pocketmine/network/mcpe/protocol/SetSpawnPositionPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,8 +28,7 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetSpawnPositionPacket extends DataPacket
-{
+class SetSpawnPositionPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::SET_SPAWN_POSITION_PACKET;
 
 	const TYPE_PLAYER_SPAWN = 0;
@@ -41,23 +40,19 @@ class SetSpawnPositionPacket extends DataPacket
 	public $z;
 	public $spawnForced;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->spawnType = $this->getVarInt();
 		$this->getBlockPosition($this->x, $this->y, $this->z);
 		$this->spawnForced = $this->getBool();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putVarInt($this->spawnType);
 		$this->putBlockPosition($this->x, $this->y, $this->z);
 		$this->putBool($this->spawnForced);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleSetSpawnPosition($this);
 	}
 
