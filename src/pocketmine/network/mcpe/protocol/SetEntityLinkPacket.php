7cb35d5fb775c36bc70273b5f9af1842ef3270dd
NycuRO <nghergu@yahoo.com>
2017-07-30 18:41:07 +0300

    Update SetEntityLinkPacket.php

--- src/pocketmine/network/mcpe/protocol/SetEntityLinkPacket.php
+++ src/pocketmine/network/mcpe/protocol/SetEntityLinkPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,31 +28,26 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetEntityLinkPacket extends DataPacket
-{
+class SetEntityLinkPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::SET_ENTITY_LINK_PACKET;
 
 	public $from;
 	public $to;
 	public $type;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->from = $this->getEntityUniqueId();
 		$this->to = $this->getEntityUniqueId();
 		$this->type = $this->getByte();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putEntityUniqueId($this->from);
 		$this->putEntityUniqueId($this->to);
 		$this->putByte($this->type);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleSetEntityLink($this);
 	}
 
