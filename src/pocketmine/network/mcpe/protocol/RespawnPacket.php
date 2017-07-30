c81f5482a4aeac9cf9a62adc61e1d2f892c8532d
NycuRO <nghergu@yahoo.com>
2017-07-30 18:37:16 +0300

    Update RespawnPacket.php

--- src/pocketmine/network/mcpe/protocol/RespawnPacket.php
+++ src/pocketmine/network/mcpe/protocol/RespawnPacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,27 +28,22 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class RespawnPacket extends DataPacket
-{
+class RespawnPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::RESPAWN_PACKET;
 
 	public $x;
 	public $y;
 	public $z;
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->getVector3f($this->x, $this->y, $this->z);
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putVector3f($this->x, $this->y, $this->z);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleRespawn($this);
 	}
 
