e7c6cdaa53b97d2579252c68e6365721e56c296f
NycuRO <nghergu@yahoo.com>
2017-07-30 18:38:42 +0300

    Update ServerToClientHandshakePacket.php

--- src/pocketmine/network/mcpe/protocol/ServerToClientHandshakePacket.php
+++ src/pocketmine/network/mcpe/protocol/ServerToClientHandshakePacket.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,33 +28,27 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class ServerToClientHandshakePacket extends DataPacket
-{
+class ServerToClientHandshakePacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::SERVER_TO_CLIENT_HANDSHAKE_PACKET;
 
 	public $publicKey;
 	public $serverToken;
 
-	public function canBeSentBeforeLogin(): bool
-	{
+	public function canBeSentBeforeLogin() : bool{
 		return true;
 	}
 
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->publicKey = $this->getString();
 		$this->serverToken = $this->getString();
 	}
 
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putString($this->publicKey);
 		$this->putString($this->serverToken);
 	}
 
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleServerToClientHandshake($this);
 	}
 }
