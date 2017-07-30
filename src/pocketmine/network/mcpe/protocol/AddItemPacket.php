a1ebb055e9f894a368a19f0fd612c69d7a1fad47
NycuRO <nghergu@yahoo.com>
2017-07-30 17:25:14 +0300

    Update AddItemPacket.php

--- src/pocketmine/network/mcpe/protocol/AddItemPacket.php
+++ src/pocketmine/network/mcpe/protocol/AddItemPacket.php
@@ -1,12 +1,11 @@
 <?php
-
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -18,36 +17,20 @@
  *
  *
 */
-
-declare(strict_types = 1);
-
+declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol;
-
 #include <rules/DataPacket.h>
-
-
 use pocketmine\network\mcpe\NetworkSession;
-
-class AddItemPacket extends DataPacket
-{
+class AddItemPacket extends DataPacket{
 	const NETWORK_ID = ProtocolInfo::ADD_ITEM_PACKET;
-
 	public $item;
-
-	public function decode()
-	{
+	public function decodePayload(){
 		$this->item = $this->getSlot();
 	}
-
-	public function encode()
-	{
-		$this->reset();
+	public function encodePayload(){
 		$this->putSlot($this->item);
 	}
-
-	public function handle(NetworkSession $session): bool
-	{
+	public function handle(NetworkSession $session) : bool{
 		return $session->handleAddItem($this);
 	}
-
 }
