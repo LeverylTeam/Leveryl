e9cd9fdca357360a9d6d150ef42815b69d8ec028
NycuRO <nghergu@yahoo.com>
2017-08-01 14:33:04 +0300

    Update BlockEntityDataPacket.php

--- src/pocketmine/network/mcpe/protocol/BlockEntityDataPacket.php
+++ src/pocketmine/network/mcpe/protocol/BlockEntityDataPacket.php
@@ -25,7 +25,6 @@ namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
 class BlockEntityDataPacket extends DataPacket
@@ -53,5 +52,4 @@ class BlockEntityDataPacket extends DataPacket
 	{
 		return $session->handleBlockEntityData($this);
 	}
-
 }
