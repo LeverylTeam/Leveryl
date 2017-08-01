5579264d06372111c60adaee14c461bb9344f0a1
NycuRO <nghergu@yahoo.com>
2017-08-01 15:19:03 +0300

    Update ResourcePackChunkRequestPacket.php

--- src/pocketmine/network/mcpe/protocol/ResourcePackChunkRequestPacket.php
+++ src/pocketmine/network/mcpe/protocol/ResourcePackChunkRequestPacket.php
@@ -19,33 +19,35 @@
  *
 */
 
-declare(strict_types=1);
-
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class ResourcePackChunkRequestPacket extends DataPacket{
+class ResourcePackChunkRequestPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::RESOURCE_PACK_CHUNK_REQUEST_PACKET;
 
 	public $packId;
 	public $chunkIndex;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->packId = $this->getString();
 		$this->chunkIndex = $this->getLInt();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putString($this->packId);
 		$this->putLInt($this->chunkIndex);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleResourcePackChunkRequest($this);
 	}
 }
