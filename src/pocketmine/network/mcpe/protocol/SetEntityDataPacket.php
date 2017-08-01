1862bf40bbcce4469f2ae8a9f889b8261e2ffa31
NycuRO <nghergu@yahoo.com>
2017-08-01 15:24:50 +0300

    Update SetEntityDataPacket.php

--- src/pocketmine/network/mcpe/protocol/SetEntityDataPacket.php
+++ src/pocketmine/network/mcpe/protocol/SetEntityDataPacket.php
@@ -19,32 +19,35 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetEntityDataPacket extends DataPacket{
+class SetEntityDataPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::SET_ENTITY_DATA_PACKET;
 
 	public $entityRuntimeId;
 	public $metadata;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->metadata = $this->getEntityMetadata();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putEntityMetadata($this->metadata);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleSetEntityData($this);
 	}
 }
