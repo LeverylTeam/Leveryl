1bf6e82cbe42cfaa3c08d552c6ef3740596f834c
NycuRO <nghergu@yahoo.com>
2017-08-01 18:13:21 +0300

    Update SourceInterface.php

--- src/pocketmine/network/SourceInterface.php
+++ src/pocketmine/network/SourceInterface.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,12 +19,11 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /**
  * Network-related classes
  */
-
 namespace pocketmine\network;
 
 use pocketmine\network\mcpe\protocol\DataPacket;
@@ -33,39 +32,37 @@ use pocketmine\Player;
 /**
  * Classes that implement this interface will be able to be attached to players
  */
-interface SourceInterface
-{
+interface SourceInterface{
 
 	/**
 	 * Sends a DataPacket to the interface, returns an unique identifier for the packet if $needACK is true
 	 *
-	 * @param Player $player
+	 * @param Player     $player
 	 * @param DataPacket $packet
-	 * @param bool $needACK
-	 * @param bool $immediate
+	 * @param bool       $needACK
+	 * @param bool       $immediate
 	 *
-	 * @return int
+	 * @return int|null
 	 */
-	public function putPacket(Player $player, DataPacket $packet, $needACK = false, $immediate = true);
+	public function putPacket(Player $player, DataPacket $packet, bool $needACK = false, bool $immediate = true);
 
 	/**
 	 * Terminates the connection
 	 *
 	 * @param Player $player
 	 * @param string $reason
-	 *
 	 */
-	public function close(Player $player, $reason = "unknown reason");
+	public function close(Player $player, string $reason = "unknown reason");
 
 	/**
 	 * @param string $name
 	 */
-	public function setName($name);
+	public function setName(string $name);
 
 	/**
 	 * @return bool
 	 */
-	public function process();
+	public function process() : bool;
 
 	public function shutdown();
 
