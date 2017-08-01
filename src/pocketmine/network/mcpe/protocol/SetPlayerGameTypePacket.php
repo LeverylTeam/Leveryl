f050f913715150376f74f45ed4b9ab6ed467cbbb
NycuRO <nghergu@yahoo.com>
2017-08-01 15:27:51 +0300

    Update SetPlayerGameTypePacket.php

--- src/pocketmine/network/mcpe/protocol/SetPlayerGameTypePacket.php
+++ src/pocketmine/network/mcpe/protocol/SetPlayerGameTypePacket.php
@@ -19,30 +19,32 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetPlayerGameTypePacket extends DataPacket{
+class SetPlayerGameTypePacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::SET_PLAYER_GAME_TYPE_PACKET;
 
 	public $gamemode;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->gamemode = $this->getVarInt();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putVarInt($this->gamemode);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleSetPlayerGameType($this);
 	}
-
 }
