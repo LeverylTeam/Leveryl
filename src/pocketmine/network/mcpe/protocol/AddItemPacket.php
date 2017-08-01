f454821136c7de4001949e082bd677c896e5f1b2
NycuRO <nghergu@yahoo.com>
2017-08-01 13:30:21 +0300

    Update AddItemPacket.php

--- src/pocketmine/network/mcpe/protocol/AddItemPacket.php
+++ src/pocketmine/network/mcpe/protocol/AddItemPacket.php
@@ -19,7 +19,7 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
@@ -28,20 +28,24 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class AddItemPacket extends DataPacket{
+class AddItemPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::ADD_ITEM_PACKET;
 
 	public $item;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->item = $this->getSlot();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putSlot($this->item);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleAddItem($this);
 	}
 
