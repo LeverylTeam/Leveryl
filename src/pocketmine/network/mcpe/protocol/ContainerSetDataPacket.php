3b01dd8d72c5b2d21e17e0b5d09d2c0287bff736
NycuRO <nghergu@yahoo.com>
2017-08-01 14:44:58 +0300

    Update ContainerSetDataPacket.php

--- src/pocketmine/network/mcpe/protocol/ContainerSetDataPacket.php
+++ src/pocketmine/network/mcpe/protocol/ContainerSetDataPacket.php
@@ -25,7 +25,6 @@ namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
 class ContainerSetDataPacket extends DataPacket
@@ -54,5 +53,4 @@ class ContainerSetDataPacket extends DataPacket
 	{
 		return $session->handleContainerSetData($this);
 	}
-
 }
