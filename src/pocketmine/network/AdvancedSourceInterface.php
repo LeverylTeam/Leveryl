a954244b0c5e9651c14d6938770c3da7ce4da88c
NycuRO <nghergu@yahoo.com>
2017-08-01 18:12:13 +0300

    Update AdvancedSourceInterface.php

--- src/pocketmine/network/AdvancedSourceInterface.php
+++ src/pocketmine/network/AdvancedSourceInterface.php
@@ -2,11 +2,11 @@
 
 /*
  *
- *  ____			_		_   __  __ _				  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___	  |  \/  |  _ \
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
  * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
  * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|	 |_|  |_|_|
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,22 +19,20 @@
  *
 */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /**
  * Network-related classes
  */
-
 namespace pocketmine\network;
 
-interface AdvancedSourceInterface extends SourceInterface
-{
+interface AdvancedSourceInterface extends SourceInterface{
 
 	/**
 	 * @param string $address
-	 * @param int $timeout Seconds
+	 * @param int    $timeout Seconds
 	 */
-	public function blockAddress($address, $timeout = 300);
+	public function blockAddress(string $address, int $timeout = 300);
 
 	/**
 	 * @param Network $network
@@ -43,9 +41,9 @@ interface AdvancedSourceInterface extends SourceInterface
 
 	/**
 	 * @param string $address
-	 * @param int $port
+	 * @param int    $port
 	 * @param string $payload
 	 */
-	public function sendRawPacket($address, $port, $payload);
+	public function sendRawPacket(string $address, int $port, string $payload);
 
 }
