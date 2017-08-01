d1c4dc496b0bd2e6c484bad1a3cf51996f696c75
NycuRO <nghergu@yahoo.com>
2017-08-01 15:27:17 +0300

    Update SetHealthPacket.php

--- src/pocketmine/network/mcpe/protocol/SetHealthPacket.php
+++ src/pocketmine/network/mcpe/protocol/SetHealthPacket.php
@@ -19,30 +19,32 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetHealthPacket extends DataPacket{
+class SetHealthPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::SET_HEALTH_PACKET;
 
 	public $health;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->health = $this->getVarInt();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putVarInt($this->health);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleSetHealth($this);
 	}
-
 }
