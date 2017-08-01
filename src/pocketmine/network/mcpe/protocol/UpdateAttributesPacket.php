6cef2c8c37530546795ca4587405f0ae50e6e2f6
NycuRO <nghergu@yahoo.com>
2017-08-01 15:36:13 +0300

    Update UpdateAttributesPacket.php

--- src/pocketmine/network/mcpe/protocol/UpdateAttributesPacket.php
+++ src/pocketmine/network/mcpe/protocol/UpdateAttributesPacket.php
@@ -19,35 +19,37 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\entity\Attribute;
 use pocketmine\network\mcpe\NetworkSession;
 
-class UpdateAttributesPacket extends DataPacket{
+class UpdateAttributesPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::UPDATE_ATTRIBUTES_PACKET;
 
 	public $entityRuntimeId;
 	/** @var Attribute[] */
 	public $entries = [];
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->entries = $this->getAttributeList();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putAttributeList(...$this->entries);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleUpdateAttributes($this);
 	}
-
 }
