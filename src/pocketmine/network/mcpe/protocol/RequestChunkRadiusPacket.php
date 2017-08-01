a3e3b12f7b85320b010435b9ca157c2dc047e965
NycuRO <nghergu@yahoo.com>
2017-08-01 15:18:04 +0300

    Update RequestChunkRadiusPacket.php

--- src/pocketmine/network/mcpe/protocol/RequestChunkRadiusPacket.php
+++ src/pocketmine/network/mcpe/protocol/RequestChunkRadiusPacket.php
@@ -19,30 +19,32 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class RequestChunkRadiusPacket extends DataPacket{
+class RequestChunkRadiusPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::REQUEST_CHUNK_RADIUS_PACKET;
 
 	public $radius;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->radius = $this->getVarInt();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putVarInt($this->radius);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleRequestChunkRadius($this);
 	}
-
 }
