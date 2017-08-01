d3bd922dc312a86109c0a6fcf59caeead71044cd
NycuRO <nghergu@yahoo.com>
2017-08-01 15:02:20 +0300

    Update MapInfoRequestPacket.php

--- src/pocketmine/network/mcpe/protocol/MapInfoRequestPacket.php
+++ src/pocketmine/network/mcpe/protocol/MapInfoRequestPacket.php
@@ -19,30 +19,32 @@
  *
 */
 
-declare(strict_types=1);
-
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class MapInfoRequestPacket extends DataPacket{
+class MapInfoRequestPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::MAP_INFO_REQUEST_PACKET;
 
 	public $mapId;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->mapId = $this->getEntityUniqueId();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putEntityUniqueId($this->mapId);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleMapInfoRequest($this);
 	}
 }
