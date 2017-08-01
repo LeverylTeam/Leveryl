d3ba982341ba3bebda1843b749e4df03127bca49
NycuRO <nghergu@yahoo.com>
2017-08-01 15:24:23 +0300

    Update SetDifficultyPacket.php

--- src/pocketmine/network/mcpe/protocol/SetDifficultyPacket.php
+++ src/pocketmine/network/mcpe/protocol/SetDifficultyPacket.php
@@ -19,30 +19,32 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class SetDifficultyPacket extends DataPacket{
+class SetDifficultyPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::SET_DIFFICULTY_PACKET;
 
 	public $difficulty;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->difficulty = $this->getUnsignedVarInt();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putUnsignedVarInt($this->difficulty);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleSetDifficulty($this);
 	}
-
 }
