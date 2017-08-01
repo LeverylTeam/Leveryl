9519a75eff57006154bde11a1d0455a7ed532151
NycuRO <nghergu@yahoo.com>
2017-08-01 15:04:11 +0300

    Update MobEquipmentPacket.php

--- src/pocketmine/network/mcpe/protocol/MobEquipmentPacket.php
+++ src/pocketmine/network/mcpe/protocol/MobEquipmentPacket.php
@@ -19,16 +19,16 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class MobEquipmentPacket extends DataPacket{
+class MobEquipmentPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::MOB_EQUIPMENT_PACKET;
 
 	public $entityRuntimeId;
@@ -37,7 +37,8 @@ class MobEquipmentPacket extends DataPacket{
 	public $hotbarSlot;
 	public $windowId = 0;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->item = $this->getSlot();
 		$this->inventorySlot = $this->getByte();
@@ -45,7 +46,8 @@ class MobEquipmentPacket extends DataPacket{
 		$this->windowId = $this->getByte();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putSlot($this->item);
 		$this->putByte($this->inventorySlot);
@@ -53,8 +55,8 @@ class MobEquipmentPacket extends DataPacket{
 		$this->putByte($this->windowId);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleMobEquipment($this);
 	}
-
 }
