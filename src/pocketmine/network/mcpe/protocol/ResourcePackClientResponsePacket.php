c325cf0734650ab073f2a045221b9d8c51eadafe
NycuRO <nghergu@yahoo.com>
2017-08-01 15:19:51 +0300

    Update ResourcePackClientResponsePacket.php

--- src/pocketmine/network/mcpe/protocol/ResourcePackClientResponsePacket.php
+++ src/pocketmine/network/mcpe/protocol/ResourcePackClientResponsePacket.php
@@ -19,16 +19,16 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class ResourcePackClientResponsePacket extends DataPacket{
+class ResourcePackClientResponsePacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::RESOURCE_PACK_CLIENT_RESPONSE_PACKET;
 
 	const STATUS_REFUSED = 1;
@@ -39,24 +39,28 @@ class ResourcePackClientResponsePacket extends DataPacket{
 	public $status;
 	public $packIds = [];
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->status = $this->getByte();
 		$entryCount = $this->getLShort();
-		while($entryCount-- > 0){
+		while ($entryCount-- > 0)
+		{
 			$this->packIds[] = $this->getString();
 		}
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putByte($this->status);
 		$this->putLShort(count($this->packIds));
-		foreach($this->packIds as $id){
+		foreach ($this->packIds as $id)
+		{
 			$this->putString($id);
 		}
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleResourcePackClientResponse($this);
 	}
-
 }
