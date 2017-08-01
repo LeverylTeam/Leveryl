1559fb92fa16e1663b25d5d4aa3dc8ee834b227d
NycuRO <nghergu@yahoo.com>
2017-08-01 15:16:40 +0300

    Update RemoveBlockPacket.php

--- src/pocketmine/network/mcpe/protocol/RemoveBlockPacket.php
+++ src/pocketmine/network/mcpe/protocol/RemoveBlockPacket.php
@@ -19,32 +19,34 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class RemoveBlockPacket extends DataPacket{
+class RemoveBlockPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::REMOVE_BLOCK_PACKET;
 
 	public $x;
 	public $y;
 	public $z;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->getBlockPosition($this->x, $this->y, $this->z);
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putBlockPosition($this->x, $this->y, $this->z);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleRemoveBlock($this);
 	}
-
 }
