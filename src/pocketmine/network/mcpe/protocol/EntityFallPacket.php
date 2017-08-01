21238c3d10d359049d2599f43e48a9c9d5988702
NycuRO <nghergu@yahoo.com>
2017-08-01 14:09:02 +0300

    Reformat File :)

--- src/pocketmine/network/mcpe/protocol/EntityFallPacket.php
+++ src/pocketmine/network/mcpe/protocol/EntityFallPacket.php
@@ -19,35 +19,38 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 
-class EntityFallPacket extends DataPacket{
+class EntityFallPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::ENTITY_FALL_PACKET;
 
 	public $entityRuntimeId;
 	public $fallDistance;
 	public $bool1;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->entityRuntimeId = $this->getEntityRuntimeId();
 		$this->fallDistance = $this->getLFloat();
 		$this->bool1 = $this->getBool();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putEntityRuntimeId($this->entityRuntimeId);
 		$this->putLFloat($this->fallDistance);
 		$this->putBool($this->bool1);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleEntityFall($this);
 	}
 }
