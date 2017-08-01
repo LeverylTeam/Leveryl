c280ffa265a75c962544ea98988b09a8a1713554
NycuRO <nghergu@yahoo.com>
2017-08-01 13:51:59 +0300

    Reformat File :)

--- src/pocketmine/network/mcpe/protocol/ContainerSetContentPacket.php
+++ src/pocketmine/network/mcpe/protocol/ContainerSetContentPacket.php
@@ -19,17 +19,17 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\network\mcpe\NetworkSession;
 use pocketmine\network\mcpe\protocol\types\ContainerIds;
 
-class ContainerSetContentPacket extends DataPacket{
+class ContainerSetContentPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::CONTAINER_SET_CONTENT_PACKET;
 
 	public $windowid;
@@ -37,44 +37,55 @@ class ContainerSetContentPacket extends DataPacket{
 	public $slots = [];
 	public $hotbar = [];
 
-	public function clean(){
+	public function clean()
+	{
 		$this->slots = [];
 		$this->hotbar = [];
 		return parent::clean();
 	}
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->windowid = $this->getUnsignedVarInt();
 		$this->targetEid = $this->getEntityUniqueId();
 		$count = $this->getUnsignedVarInt();
-		for($s = 0; $s < $count and !$this->feof(); ++$s){
+		for($s = 0; $s < $count and !$this->feof(); ++$s)
+		{
 			$this->slots[$s] = $this->getSlot();
 		}
 
 		$hotbarCount = $this->getUnsignedVarInt(); //MCPE always sends this, even when it's not a player inventory
-		for($s = 0; $s < $hotbarCount and !$this->feof(); ++$s){
+		for($s = 0; $s < $hotbarCount and !$this->feof(); ++$s)
+		{
 			$this->hotbar[$s] = $this->getVarInt();
 		}
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putUnsignedVarInt($this->windowid);
 		$this->putEntityUniqueId($this->targetEid);
 		$this->putUnsignedVarInt(count($this->slots));
-		foreach($this->slots as $slot){
+		foreach($this->slots as $slot)
+		{
 			$this->putSlot($slot);
 		}
-		if($this->windowid === ContainerIds::INVENTORY and count($this->hotbar) > 0){
+		if($this->windowid === ContainerIds::INVENTORY and count($this->hotbar) > 0)
+		{
 			$this->putUnsignedVarInt(count($this->hotbar));
-			foreach($this->hotbar as $slot){
+			foreach($this->hotbar as $slot)
+			{
 				$this->putVarInt($slot);
 			}
-		}else{
+		}
+		else
+		{
 			$this->putUnsignedVarInt(0);
 		}
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleContainerSetContent($this);
 	}
 
