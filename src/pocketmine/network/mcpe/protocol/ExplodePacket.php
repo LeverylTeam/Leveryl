c4a123ed9078db07882c64c514ecbffa6d4621e0
NycuRO <nghergu@yahoo.com>
2017-08-01 14:55:25 +0300

    Update ExplodePacket.php

--- src/pocketmine/network/mcpe/protocol/ExplodePacket.php
+++ src/pocketmine/network/mcpe/protocol/ExplodePacket.php
@@ -51,7 +51,7 @@ class ExplodePacket extends DataPacket
 		$this->getVector3f($this->x, $this->y, $this->z);
 		$this->radius = (float) ($this->getVarInt() / 32);
 		$count = $this->getUnsignedVarInt();
-		for($i = 0; $i < $count; ++$i)
+		for ($i = 0; $i < $count; ++$i)
 		{
 			$x = $y = $z = null;
 			$this->getSignedBlockPosition($x, $y, $z);
@@ -64,9 +64,9 @@ class ExplodePacket extends DataPacket
 		$this->putVector3f($this->x, $this->y, $this->z);
 		$this->putVarInt((int) ($this->radius * 32));
 		$this->putUnsignedVarInt(count($this->records));
-		if(count($this->records) > 0)
+		if (count($this->records) > 0)
 		{
-			foreach($this->records as $record)
+			foreach ($this->records as $record)
 			{
 				$this->putSignedBlockPosition($record->x, $record->y, $record->z);
 			}
