e6ac51343707204802da1bbd55585b759be21d53
NycuRO <nghergu@yahoo.com>
2017-08-01 14:05:53 +0300

    Update DropItemPacket.php

--- src/pocketmine/network/mcpe/protocol/DropItemPacket.php
+++ src/pocketmine/network/mcpe/protocol/DropItemPacket.php
@@ -19,34 +19,37 @@
  *
 */
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace pocketmine\network\mcpe\protocol;
 
 #include <rules/DataPacket.h>
 
-
 use pocketmine\item\Item;
 use pocketmine\network\mcpe\NetworkSession;
 
-class DropItemPacket extends DataPacket{
+class DropItemPacket extends DataPacket
+{
 	const NETWORK_ID = ProtocolInfo::DROP_ITEM_PACKET;
 
 	public $type;
 	/** @var Item */
 	public $item;
 
-	public function decodePayload(){
+	public function decodePayload()
+	{
 		$this->type = $this->getByte();
 		$this->item = $this->getSlot();
 	}
 
-	public function encodePayload(){
+	public function encodePayload()
+	{
 		$this->putByte($this->type);
 		$this->putSlot($this->item);
 	}
 
-	public function handle(NetworkSession $session) : bool{
+	public function handle(NetworkSession $session) : bool
+	{
 		return $session->handleDropItem($this);
 	}
 
