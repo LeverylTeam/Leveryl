da8e7cf0bcfbe4676f4c825e4220914654cacf62
CortexPE <mcpe4life62@gmail.com>
2017-08-02 01:20:24 -0500

    Fix MultiProtocol after Revert

--- src/pocketmine/network/mcpe/protocol/LoginPacket.php
+++ src/pocketmine/network/mcpe/protocol/LoginPacket.php
@@ -70,7 +70,7 @@ class LoginPacket extends DataPacket
 	{
 		$this->protocol = $this->getInt();
 
-		if($this->protocol !== ProtocolInfo::CURRENT_PROTOCOL) {
+		if(!in_array($this->protocol, ProtocolInfo::ACCEPTED_PROTOCOLS)) {
 			$this->buffer = null;
 
 			return; //Do not attempt to decode for non-accepted protocols
