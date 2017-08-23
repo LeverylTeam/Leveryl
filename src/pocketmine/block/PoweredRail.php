d38437ed0e463b7d4ebd824c0c1668b85e7d3d73
Ip <jun040329@gmail.com>
2017-08-23 23:44:54 +0900

    Add files via upload

---
+++ src/pocketmine/block/PoweredRail.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ *   ____  _            _      _       _     _
+ *  |  _ \| |          | |    (_)     | |   | |
+ *  | |_) | |_   _  ___| |     _  __ _| |__ | |_
+ *  |  _ <| | | | |/ _ \ |    | |/ _` | '_ \| __|
+ *  | |_) | | |_| |  __/ |____| | (_| | | | | |_
+ *  |____/|_|\__,_|\___|______|_|\__, |_| |_|\__|
+ *                                __/ |
+ *                               |___/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author BlueLightJapan Team
+ * 
+*/
+
+namespace pocketmine\block;
+
+class PoweredRail extends Rail{
+	protected $id = self::POWERED_RAIL;
+
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	public function getName(){
+		return "Powered Rail";
+	}
+
+	public function isPowered(){
+		return true;//TODO
+	}
+}
