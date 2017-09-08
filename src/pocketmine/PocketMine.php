105bb31e0f84562b5f71e554d285d78cb19ea35a
CortexPE <CortexPE@users.noreply.github.com>
2017-09-08 05:47:54 -0500

    hm

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -91,7 +91,7 @@ namespace pocketmine {
 		@define('pocketmine\PATH', \getcwd() . DIRECTORY_SEPARATOR);
 	}
 
-	if(version_compare(phpversion(), "7.0", ">")){
+	if(version_compare(PHP_VERSION, '7.2.0', '>=')){
 		echo "[CRITICAL] You must use PHP 7.0.x" . PHP_EOL;
 		echo "[CRITICAL] If you want to use PHP 7.2, Use this branch instead: https://github.com/LeverylTeam/Leveryl/tree/php7.2" . PHP_EOL;
 		echo PHP_EOL;
